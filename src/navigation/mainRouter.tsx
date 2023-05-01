221823f81a6903bc239b133bca9e1963a86fbe6a
ProkoptsovD <fenderman1992@gmail.com>
2023-05-01 08:34:26 +0300

    add basename to router

--- src/navigation/mainRouter.tsx
+++ src/navigation/mainRouter.tsx
@@ -13,26 +13,29 @@ const HomePage = React.lazy(() => import('@pages/HomePage'));
 const CityPage = React.lazy(() => import('@pages/SingleCityPage'));
 
 export function MainRouter() {
-  const router = createBrowserRouter([
-    {
-      path: ROUTER_KEYS.HOME,
-      element: <SharedLayout />,
-      children: [
-        {
-          path: ROUTER_KEYS.HOME,
-          element: <HomePage />
-        },
-        {
-          path: ROUTER_KEYS.CITY + '/:id',
-          element: <CityPage />
-        }
-      ]
-    },
-    {
-      path: '*',
-      element: <Page404 />
-    }
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: ROUTER_KEYS.HOME,
+        element: <SharedLayout />,
+        children: [
+          {
+            path: ROUTER_KEYS.HOME,
+            element: <HomePage />
+          },
+          {
+            path: ROUTER_KEYS.CITY + '/:id',
+            element: <CityPage />
+          }
+        ]
+      },
+      {
+        path: '*',
+        element: <Page404 />
+      }
+    ],
+    { basename: '/weather-forecast-app' }
+  );
 
   return <RouterProvider router={router} />;
 }
